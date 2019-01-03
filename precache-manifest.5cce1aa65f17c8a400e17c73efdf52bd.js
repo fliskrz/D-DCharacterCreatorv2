303a51316e887f460d0393b55759c3dbdfd49b09
fliskrz <flis.krz@gmail.com>
2019-01-03 02:43:08 +0100

    Updates

---
+++ precache-manifest.5cce1aa65f17c8a400e17c73efdf52bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7476512ccdfce7bf2d56",
+    "url": "/D-DCharacterCreatorv2/static/js/runtime~main.7476512c.js"
+  },
+  {
+    "revision": "98160fdaa746c99b01b7",
+    "url": "/D-DCharacterCreatorv2/static/js/main.98160fda.chunk.js"
+  },
+  {
+    "revision": "888378eb6e2605cd5172",
+    "url": "/D-DCharacterCreatorv2/static/js/1.888378eb.chunk.js"
+  },
+  {
+    "revision": "98160fdaa746c99b01b7",
+    "url": "/D-DCharacterCreatorv2/static/css/main.9f677885.chunk.css"
+  },
+  {
+    "revision": "4e348270ae7fdec1be51a275d93c6457",
+    "url": "/D-DCharacterCreatorv2/index.html"
+  }
+];
